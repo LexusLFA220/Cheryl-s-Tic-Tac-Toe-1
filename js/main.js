dc4217cd77815def0d67fa46af6157987ee1dbee
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-02-22 15:23:33 -0500

    Make your mark

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 /*----- app's state (variables) -----*/
 let board;
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
 /*----- functions -----*/
 function init() {
@@ -10,6 +11,16 @@ function init() {
     '', '', '',
     '', '', ''
     ];
+    // new code here
+    render();
     };
     //be sure to call the init function!
-    init();
+    
+    function render() {
+        board.forEach(function(mark, index) {
+        //this sets the text content of the square of the same position to the mark on the board. 
+        squares[index].textContent = mark;
+        });
+        };
+        init();
+    
