ddd031e52385da81bb938e39734d705f228c95dc
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-02-22 15:31:18 -0500

    Take Turns X and O

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = 'X';
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
+const messages = document.querySelector('h2');
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
+function handleTurn() {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    // This is tidy
+    turn = turn === 'X' ? 'O' : 'X';
+    
+    render();
+    };
 function init() {
     board = [
     '', '', '',
     '', '', '',
     '', '', ''
     ];
-    // new code here
+    
     render();
     };
-    //be sure to call the init function!
+    
     
     function render() {
-        board.forEach(function(mark, index) {
-        //this sets the text content of the square of the same position to the mark on the board. 
-        squares[index].textContent = mark;
+        board.forEach(function(val, idx) {
+        squares[idx].textContent = val;
         });
+        
+        messages.textContent = `It's ${turn}'s turn!`;
         };
         init();
     
