0f6a39df3837b650bd8aab4654f66f1590444af4
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-02-22 16:12:50 -0500

    Reset Button Works

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ const squares = Array.from(document.querySelectorAll('#board div'));
 const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
+document.getElementById('reset-button').addEventListener('click', init);
 /*----- functions -----*/
 function getWinner() {
     let winner = null;
