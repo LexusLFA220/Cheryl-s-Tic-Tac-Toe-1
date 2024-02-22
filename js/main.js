0417ab660c084ea3cf045ceeafdcdb4571146513
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-02-22 14:36:06 -0500

    3x3 Grid Created

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+/*----- functions -----*/
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+    };
+    //be sure to call the init function!
+    init();
