743d1464035ef765e8650db5d24a148eabecc328
LexusLFA220 <alex.amin@lc.cuny.edu>
2024-02-22 15:58:54 -0500

    Win Logic Refactored

--- js/main.js
+++ js/main.js
@@ -1,23 +1,41 @@
 /*----- constants -----*/
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], 
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 /*----- app's state (variables) -----*/
 let board;
 let turn = 'X';
+let win;
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll('#board div'));
 const messages = document.querySelector('h2');
 /*----- event listeners -----*/
 document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
-function handleTurn() {
-    let idx = squares.findIndex(function(square) {
-    return square === event.target;
-    });
-    board[idx] = turn;
-    // This is tidy
-    turn = turn === 'X' ? 'O' : 'X';
-    
-    render();
+function getWinner() {
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
+      });
+      return winner;
     };
+
+    function handleTurn(event) {
+        let idx = squares.findIndex(function(square) {
+        return square === event.target;
+        });
+        board[idx] = turn;
+        turn = turn === 'X' ? 'O' : 'X';
+        win = getWinner();
+        render();
+        };
 function init() {
     board = [
     '', '', '',
@@ -34,7 +52,7 @@ function init() {
         squares[idx].textContent = val;
         });
         
-        messages.textContent = `It's ${turn}'s turn!`;
+        messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
         };
         init();
     
